f74ecb993636ec2e8623b38353797c26318dcced
Arthur Samuel Hack <arthur.hack.live@live.com>
2025-05-07 11:33:32 -0300

    Added connection

---
+++ src/01-connection/connection.ts
@@ -0,0 +1,24 @@
+import * as amqp from 'amqplib';
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function connect() {
+    try {
+        const connection = await amqp.connect('amqp://admin:admin@localhost:5672');
+        console.log('Conectato ao RabbitMQ com sucesso!');
+
+        const channel = await connection.createChannel();
+        console.log('Canal criado com sucesso!');
+        
+        await sleep(30000);
+
+        await channel.close();
+        await connection.close();
+        console.log('Conexão fechada');
+
+    } catch (error) {
+        console.error('Erro na conexão com RabbitMQ');
+    }
+}
+
+connect();
